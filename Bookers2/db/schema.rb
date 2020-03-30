304bdf895ff218caca40489572de41b6af6261e0
YUI ISHIHARA <yui@YUInoMacBook-Air.local>
2020-03-30 16:25:58 +0900

    Add Mypage

--- Bookers2/db/schema.rb
+++ Bookers2/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_034345) do
+ActiveRecord::Schema.define(version: 2020_03_30_042613) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
-    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_03_29_034345) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
